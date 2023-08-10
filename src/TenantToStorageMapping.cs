065a2f7fc2ab95c5c7d54e7e52206a0bacb12202
becheng <ben.cheng@gmail.com>
2023-08-09 22:49:12 -0400

    updated model

--- src/TenantToStorageMapping.cs
+++ src/TenantToStorageMapping.cs
@@ -15,15 +15,15 @@ public class TenantToStorageMapping : ITableEntity
     public Guid? CxTenantId { get; set; }
     public string? CxTenantName { get; set; }
     public StorageType StorageType { get; set; }
-    public string? ConnectionUriKVSecretName { get; set; }
-    public string? AzAccountName { get; set; }
+    public string? StorageAccessKeySecretRef { get; set; }
+    public string? StorageIdentifier { get; set; }
     public string? ContainerName { get; set; }
-    public ConnectionUriType ConnectionUriType { get; set; }
+    public ConnectionType ConnectionType { get; set; }
     public string? StorageRegion { get; set; }
     public bool IsAzureCrossTenant{ get; set; }
     public string? AzureCrossTenantId{ get; set; }
     public string? AzureCrossTenantDomain{ get; set; }
 }
 
-public enum StorageType { AzureStorageAccount, AmazonS3 };
-public enum ConnectionUriType { ConnectionString, SasUri, ContainerUri, AzSasUri, AWS3PresignedUrl };
+public enum StorageType { None, AzStorage, AwsS3 };
+public enum ConnectionType { None, AzConnectionString, AzSasUri, AzStorageSharedKey, AzOauth, AwsCredentials };
